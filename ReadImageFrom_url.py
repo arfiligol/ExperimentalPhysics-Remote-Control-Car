415d64d3fb3c4f1ad6f8891a17f141213c69becf
arfiligol <arfiligol@gmail.com>
2022-05-10 06:40:20 +0800

    Read the image from the url by opencv

---
+++ ReadImageFrom_url.py
@@ -0,0 +1,11 @@
+import cv2 # 匯入opencv函式庫
+
+
+cap = cv2.VideoCapture("http://192.168.50.219:81/stream") # Instantce an object of VideoCapture, get the video (image) from an url.
+
+while True: 
+    if (cap.isOpened()): # If the cap is normally could be use, then...
+        ret, img = cap.read() # Read an img from the source, this read() function will return two value. One is the imge data (probably five dimension data), the other is Bool value that whether the image could be read.
+        
+        cv2.imshow("image", img) # Show the image, with the window title "image", and with the data store in "img" variable.
+        cv2.waitKey(1) # Wait for... (up to you).
